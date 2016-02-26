b0216a0020619286f1e10fd3dbceb7e45ceb6202
Akash Manohar J <akash@akash.im>
2016-02-27 00:16:28 +0530

    create occurence

--- web/controllers/error_api_controller.ex
+++ web/controllers/error_api_controller.ex
@@ -5,12 +5,21 @@ defmodule ErrorCop.ErrorApiController do
   def errors(conn, params) do
     IO.inspect params
     project = Repo.get! ErrorCop.Project, params["api_key"]
-    log_hash = calculate_log_hash(params["error"]["message"], params["error"]["stacktrace"])
-    log = %ErrorCop.Log{
-      message: params["error"]["message"],
-      stacktrace: params["error"]["stack_trace"],
-      environment: params["error"]["environment"],
-    }
+    log_hash = calculate_log_hash params["error"]["message"], params["error"]["stacktrace"]
+    log = Repo.get_by ErrorCop.Log, log_hash: log_hash
+    case log do
+      nil ->
+        log = %ErrorCop.Log{
+          message: params["error"]["message"],
+          stacktrace: params["error"]["stack_trace"],
+          environment: params["error"]["environment"],
+        }
+        {:ok, log} = Repo.insert log
+        {:ok, occurrence} = Repo.insert %ErrorLog.Occurrence{log_id: log.id}
+      log ->
+        Repo.insert %ErrorCop.Occurance{log_id: log.id}
+    end
+
     json conn, %{status: :ok}
   end
 
